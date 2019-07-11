82ec36ab0a5b5248ad2093880a7ce81498defb7c
LHA <anh.lehoang@vietis.com.vn>
2019-07-11 15:42:11 +0700

    fix url

--- ebhouse/src/environments/environment.prod.ts
+++ ebhouse/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment =  {
 //  baseUrl : "http://18.139.124.34:8888/ebhouse"
 //  baseUrl : "http://localhost:8084/CapstoneProject/",
 // baseUrl:"http://ebhouse-backup.cloudjiffy.net/eb"
-baseUrl : "http://54.255.165.53:8080/ebhouse"
+// baseUrl : "http://54.255.165.53:8080/ebhouse"
+baseUrl : "http://52.196.206.109:8080/ebhouse"
 };
