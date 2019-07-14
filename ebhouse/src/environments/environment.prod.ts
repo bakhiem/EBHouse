53f7fc55c23e592d5c9b3b55b6d47013596dfa75
LHA <anh.lehoang@vietis.com.vn>
2019-07-14 15:02:22 +0700

    fix ssl

--- ebhouse/src/environments/environment.prod.ts
+++ ebhouse/src/environments/environment.prod.ts
@@ -6,5 +6,6 @@ export const environment =  {
 //  baseUrl : "http://54.255.165.53:8080/ebhouse"
 // baseUrl : "http://52.196.206.109:8080/ebhouse"
 // baseUrl : "http://localhost:8080/ebhouse"
-baseUrl : "http://54.250.72.117/backend/ebhouse/"
+// baseUrl : "http://54.250.72.117/backend/ebhouse/"
+baseUrl : "https://ebhouse.cf/backend/ebhouse/"
 };
--- ebhouse/src/environments/environment.ts
+++ ebhouse/src/environments/environment.ts
@@ -13,7 +13,8 @@ export const environment = {
   // baseUrl : "http://54.255.165.53:8080/ebhouse"
   // baseUrl : "http://ebhouse-backup.cloudjiffy.net/eb",
   // baseUrl : "http://52.196.206.109:8080/ebhouse"
-  baseUrl : "http://54.250.72.117/backend/ebhouse/"
+  // baseUrl : "http://54.250.72.117/backend/ebhouse/"
+  baseUrl : "https://ebhouse.cf/backend/ebhouse/"
 };
 
 /*
