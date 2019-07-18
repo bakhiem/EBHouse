2df772a77f83d9e517d4b8b42ec4662731679ec8
unknown <anh.lehoang@vietis.com.vn>
2019-07-18 22:07:29 +0700

    update

--- ebhouse/src/environments/environment.ts
+++ ebhouse/src/environments/environment.ts
@@ -14,13 +14,13 @@ export const environment = {
   // baseUrl : "http://ebhouse-backup.cloudjiffy.net/eb",
   // baseUrl : "http://52.196.206.109:8080/ebhouse"
   // baseUrl : "http://54.250.72.117/backend/ebhouse/"
-  baseUrl : "https://ebhouse.cf/backend/ebhouse/"
+  // baseUrl : "https://ebhouse.cf/backend/ebhouse/"
   // baseUrl : "http://ec2-54-255-244-234.ap-southeast-1.compute.amazonaws.com:8888/ebhouse"
   // baseUrl : "http://localhost:8080/CapstoneProject"
   // baseUrl : "http://localhost:8084/CapstoneProject"
   // baseUrl : "http://localhost:8888/CapstoneProject"
   // baseUrl : "http://ebhouse-backup.kilatiron.com/eb"
-  // baseUrl : "https://ebhouse.cf/backend/ebhouse"
+  baseUrl : "https://ebhouse.cf/backend/ebhouse"
   // baseUrl : "http://ebhouse-backup.cloudjiffy.net/eb",
   // baseUrl : " http://18.138.199.164/"
 
